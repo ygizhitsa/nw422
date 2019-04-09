ca6cf100b36965a08c09e8999fd2c380b1e51d06
ygizhitsa <ygizhitsa@msn.com>
2019-04-09 05:12:24 -0400

    Init week 2

---
+++ hw2/jump-start-bank-v003.py
@@ -0,0 +1,70 @@
+# Jump-Start for the Bank Marketing Study
+# as described in Marketing Data Science: Modeling Techniques
+# for Predictive Analytics with R and Python (Miller 2015)
+
+# jump-start code revised by Thomas W. Milller (2018/10/07)
+
+# Scikit Learn documentation for this assignment:
+# http://scikit-learn.org/stable/auto_examples/classification/
+#   plot_classifier_comparison.html
+# http://scikit-learn.org/stable/modules/generated/
+#   sklearn.naive_bayes.BernoulliNB.html#sklearn.naive_bayes.BernoulliNB.score
+# http://scikit-learn.org/stable/modules/generated/
+#   sklearn.linear_model.LogisticRegression.html
+# http://scikit-learn.org/stable/modules/model_evaluation.html 
+# http://scikit-learn.org/stable/modules/generated/
+#  sklearn.model_selection.KFold.html
+
+# prepare for Python version 3x features and functions
+# comment out for Python 3.x execution
+# from __future__ import division, print_function
+# from future_builtins import ascii, filter, hex, map, oct, zip
+
+# seed value for random number generators to obtain reproducible results
+RANDOM_SEED = 1
+
+# import base packages into the namespace for this program
+import numpy as np
+import pandas as pd
+
+# initial work with the smaller data set
+bank = pd.read_csv('bank.csv', sep = ';')  # start with smaller data set
+# examine the shape of original input data
+print(bank.shape)
+
+# drop observations with missing data, if any
+bank.dropna()
+# examine the shape of input data after dropping missing data
+print(bank.shape)
+
+# look at the list of column names, note that y is the response
+list(bank.columns.values)
+
+# look at the beginning of the DataFrame
+bank.head()
+
+# mapping function to convert text no/yes to integer 0/1
+convert_to_binary = {'no' : 0, 'yes' : 1}
+
+# define binary variable for having credit in default
+default = bank['default'].map(convert_to_binary)
+
+# define binary variable for having a mortgage or housing loan
+housing = bank['housing'].map(convert_to_binary)
+
+# define binary variable for having a personal loan
+loan = bank['loan'].map(convert_to_binary)
+
+# define response variable to use in the model
+response = bank['response'].map(convert_to_binary)
+
+# gather three explanatory variables and response into a numpy array 
+# here we use .T to obtain the transpose for the structure we want
+model_data = np.array([np.array(default), np.array(housing), np.array(loan), 
+    np.array(response)]).T
+
+# examine the shape of model_data, which we will use in subsequent modeling
+print(model_data.shape)
+
+# the rest of the program should set up the modeling methods
+# and evaluation within a cross-validation design
